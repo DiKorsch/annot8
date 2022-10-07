06ecc57c135f1af66ba5887bdb1bb4dfa3a643f6
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-10-07 14:16:54 +0200

    moved some App components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="pycs2">
-    <Drawer/>
+    <core-AppDrawer/>
 
     <v-main>
       <v-container fluid>
@@ -8,22 +8,14 @@
       </v-container>
     </v-main>
 
-    <Footer/>
+    <core-AppFooter/>
   </v-app>
 </template>
 
 <script>
-import Drawer from "@/components/DrawerComponent"
-import Footer from "@/components/FooterComponent"
 
 export default {
   name: 'App',
-
-  components: {
-    Drawer,
-    Footer,
-
-  },
 
   data: () => ({
   }),
