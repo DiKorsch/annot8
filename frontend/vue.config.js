e6a64482b2e3d1cf255ab8bb3d790d96f30b9b5b
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-10-07 13:17:15 +0200

    updated some font dependencies

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,17 @@
+const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  chainWebpack: config => {
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag, path, component }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
+  },
+  ...defineConfig({
+    transpileDependencies: [
+      'vuetify'
+    ]
+  })
 }
