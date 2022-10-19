a48d0d972542ad79ec00a16d48018c446c919dc7
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-10-19 15:07:48 +0200

    updated DB config parsing and added a user fixture for an admin account

--- backend/annot8/settings/database.py
+++ backend/annot8/settings/database.py
@@ -1,8 +1,29 @@
 import os
+import logging
+import configparser
+
 from annot8.settings.base import BASE_DIR
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+config_file = BASE_DIR / "mysql.cnf"
+if config_file.is_file():
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    HOST = config["client"]["host"]
+    PORT = config["client"]["port"]
+    DB = config["client"]["database"]
+    USER = config["client"]["user"]
+    PW = config["client"]["password"]
+else:
+    logging.warning("No mysql config file was found. Settings will be read from environment variables")
+    HOST = os.environ.get("MYSQL_DATABASE")
+    PORT = os.environ.get("MYSQL_PORT", 3306)
+    DB = os.environ.get("MYSQL_DATABASE")
+    USER = os.environ.get("MYSQL_USER")
+    PW = os.environ.get("MYSQL_PASSWORD")
 
 DATABASES = {
     'default': {
@@ -10,10 +31,11 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
-        'NAME': os.environ.get("MYSQL_DATABASE"),
-        'USER': os.environ.get("MYSQL_USER"),
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
-        'PORT': 3306,
+        'HOST': HOST,
+        'NAME': DB,
+        'USER': USER,
+        'PASSWORD': PW,
+        'PORT': PORT,
         'TEST': {
             "NAME": "annot8_tests"
         }
