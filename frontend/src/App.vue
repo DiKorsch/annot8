96ce32198e2cb0197f2f7438672b51c531239fe0
Dimitri Korsch <korschdima@gmail.com>
2024-01-03 10:49:36 +0100

    minor change in data property of App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,11 +17,9 @@
 export default {
   name: 'App',
 
-  data() {
-    return {
+  data: () => ({
       windowHeight: window.innerHeight
-    }
-  },
+  }),
 
   mounted() {
     this.$nextTick(() => {
